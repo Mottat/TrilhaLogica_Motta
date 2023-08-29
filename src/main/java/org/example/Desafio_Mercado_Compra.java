eae45f1755457847ef382b95bdbbb7136810581d
Thiago Motta <thiago.fmotta@gmail.com>
2023-08-29 11:06:33 -0300

    Primeiro Commit

---
+++ src/main/java/org/example/Desafio_Mercado_Compra.java
@@ -0,0 +1,43 @@
+package org.example;
+
+/*
+Você irá escrever um programa de console para um mercado que vende apenas dois tipos de bebida:
+ cerveja, refrigerante e água.
+ O programa deve perguntar qual tipo de bebida a pessoa deseja comprar e
+ caso ela opte pela cerveja o programa deve perguntar a idade da pessoa.
+ Caso a pessoa seja menor de idade deve printar "compra negada".
+ Em todos os outros casos deve printar "compra efetuada com sucesso".
+ */
+
+import java.util.Scanner;
+
+public class Desafio_Mercado_Compra {
+    public static void main(String[] args) {
+        System.out.println("###################################");
+        System.out.println("#  Bebidas disponiveis no mercado #");
+        System.out.println("#  - Cerveja                      #");
+        System.out.println("#  - Refrigerante                 #");
+        System.out.println("#  - Agua                         #");
+        System.out.println("###################################");
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Qual bebida deseja comprar? ");
+        String bebida = teclado.nextLine();
+        int idade = 0;
+        if (bebida.contains("Cerveja") || bebida.contains("cerveja")) {
+            System.out.print("Qual sua idade? ");
+            idade = teclado.nextInt();
+            if (idade < 18) {
+                System.out.print("Compra Negada");
+            } else if (idade >= 18) {
+                System.out.println("Compra efetuada com sucesso");
+            }
+        } else if (bebida.contains("Refrigerante") || bebida.contains("refrigerante") ||
+                bebida.contains("Agua") ||bebida.contains("agua") ){
+            System.out.println("Compra efetuada com sucesso");
+        } else {
+            System.out.println("Item não encontrado");
+        }
+    }
+}
+
---
+++ src/main/java/org/example/Desafio_Mercado_Compra_com_Desconto.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Desafio_Mercado_Compra_com_Desconto {
+    public static void main(String[] args) {
+
+    }
+}
