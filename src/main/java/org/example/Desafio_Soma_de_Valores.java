1090000a11830d65940a612a01a648792624a0ea
Thiago Motta <thiago.fmotta@gmail.com>
2023-09-05 17:34:11 -0300

    Desafio Soma de Valores

---
+++ src/main/java/org/example/Desafio_Soma_de_Valores.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Desafio_Soma_de_Valores {
+    public static void main(String[] args) {
+        int num=1, s=0;
+
+        while (num <=100){
+            s += num;
+            num++;
+//            System.out.println("Numero 1 - " + num);
+        }
+        System.out.println("Soma total dos numeros interios entre 1 e 100 é:  " + s);
+    }
+}
