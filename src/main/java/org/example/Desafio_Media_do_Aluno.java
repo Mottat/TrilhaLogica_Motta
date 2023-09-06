fd7ad7f67a5c18e2a7666e75bc0748371af5abad
Thiago Motta <thiago.fmotta@gmail.com>
2023-09-06 16:48:47 -0300

    Desafio Media do Aluno

---
+++ src/main/java/org/example/Desafio_Media_do_Aluno.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Desafio_Media_do_Aluno {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int n1, n2, media;
+        String resp, aluno;
+
+        do {
+            System.out.print("\nDigite o nome do aluno: ");
+            aluno = teclado.next();
+            System.out.print("\nDigite a primeira nota do aluno: ");
+            n1 = teclado.nextInt();
+            System.out.print("\nDigite a primeira nota do aluno: ");
+            n2 = teclado.nextInt();
+
+            media = (n1 + n2) / 2;
+
+//            System.out.print("\nA média do aluno(a) " + aluno + " foi de: "+ media);
+
+            if (media >=7){
+                System.out.println("\nAluno(a) " + aluno + " aprovado com média: "+ media);
+            }else {
+                System.out.println("\nAluno(a) " + aluno + " reprovado com média: "+ media);
+            }
+
+            System.out.print("\nDeseja continua? ");
+            resp = teclado.next();
+        }while (resp.equals("S"));
+    }
+}
