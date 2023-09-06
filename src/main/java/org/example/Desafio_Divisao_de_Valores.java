39db27e2becb96cba4a900dac80c5e171a8fdb4b
Thiago Motta <thiago.fmotta@gmail.com>
2023-09-06 16:33:47 -0300

    Desafio Divisao de Valores

---
+++ src/main/java/org/example/Desafio_Divisao_de_Valores.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Desafio_Divisao_de_Valores {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int n1=0, n2=0, resp=0, resto=0;
+        String res = "";
+
+        do {
+            System.out.print("\nDigite o primeiro número: ");
+            n1 = teclado.nextInt();
+            System.out.print("\nDigite o segundo número: ");
+            n2 = teclado.nextInt();
+
+            if (n2 > 0){
+                resp = n1 / n2;
+                resto = n1 % n2;
+//            System.out.println("\n Número valido");
+//                System.out.println("\nA divisão de " + n1 + " por " + n2 + " é: " + resp);
+                System.out.println("\nO resto da divisão de " + n1 + " por " + n2 + " é: " + resto);
+            } else {
+                System.out.println("\nInformar um número valido");
+            }
+            System.out.print("\nDeseja continuar? ");
+            res = teclado.next();
+
+        }while (res.equals("S"));
+    }
+}
