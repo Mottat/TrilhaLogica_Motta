e5095acf0af2e579d14ddb0a02ada682e3bdc1b5
Thiago Motta <thiago.fmotta@gmail.com>
2023-09-13 16:55:29 -0300

    Ajuste e correção de BUGs V3

--- src/main/java/org/example/Desafio_Busca_Nome.java
+++ src/main/java/org/example/Desafio_Busca_Nome.java
@@ -6,7 +6,7 @@ public class Desafio_Busca_Nome {
     public static void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
-        String nomes[] = new String[3];
+        String nomes[] = new String[10];
         String novonome = "";
 
         // Inserir clientes no vetor
