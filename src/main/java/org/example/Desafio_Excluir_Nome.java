1e07ba261d8a7fa67637471d57361a4f6a2e1c4e
Thiago Motta <thiago.fmotta@gmail.com>
2023-09-14 13:54:57 -0300

    Desafio Excluir Nome

--- src/main/java/org/example/Desafio_Excluir_Nome.java
+++ src/main/java/org/example/Desafio_Excluir_Nome.java
@@ -8,29 +8,38 @@ public class Desafio_Excluir_Nome {
     public static void main(String[] args) {
 
         String[] names = {"Ana","Tiago","Gustavo","Rafaela","Manuela","Maria","Lauro","Marcos","Carlos","Leonardo",
-                "Bruno","Rafael","Samuel","Carla","Paula","Cesar","Emerson","Gabriel","Tiago","Ana"};
+                "Bruno","Rafael","Samuel","Carla","Paula","Manuela","Rafaela","Gustavo","Tiago","Ana"};
 
-//        String[] r = removerduplicados(names);
+        String[] rem = removerduplicados(names);
 
-        System.out.println("Vetor Original: ");
+        System.out.println("Nomes do vetor Original: ");
         System.out.println(Arrays.toString(names) + " ");
+
+        System.out.println("Nomes que restaram após exclusão: ");
+        System.out.println(Arrays.toString(rem) + " ");
 
     }
 
-//    private static String[] removerduplicados(String[] names) {
-//
-//        int n = names.length;
-//        String[] r = Arrays.copyOf(names, n);
-//
-//        for (int i=0; i < n; i++){
-//            int k = i + 1;
-//            for (int j=i; j < n; j++){
-//                if (r[j] == ){
-//
-//                }
-//            }
-//        }
-//
-//        return r;
+    private static String[] removerduplicados(String[] names) {
 
+        int n = names.length;
+        String[] rem = Arrays.copyOf(names, n);
+
+        for (int i = 0; i < n; i++) {
+            int k = i + 1;
+            int removidos = 0;
+            for (int j = i + 1; j < n; j++) {
+                if (rem[j] == rem[i]) {
+                    removidos++;
+                } else {
+                    rem[k++] = rem[j];
+                }
+            }
+            n = n - removidos;
+        }
+
+        rem = Arrays.copyOf(rem, n);
+
+        return rem;
+    }
 }
